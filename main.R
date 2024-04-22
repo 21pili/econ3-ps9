02bbfd6a918e8ee881204000df12dab655a89e3e
21pili <90769845+21pili@users.noreply.github.com>
2024-04-22 17:33:40 +0200

    New commit

--- main.R
+++ main.R
@@ -13,6 +13,7 @@ library("margins")
 library("sandwich")
 library("foreign")
 library("Greg")
+library("stargazer")
 
 ### Imports
 
@@ -23,5 +24,21 @@ df <- read.dta("titanic.dta")
 ### a
 
 ols_a <- lm(survived ~ female, df)
+stargazer(ols_a, type = "latex",
+                    out = "OUTPUT/ols_a.tex",
+                    label = "olsa",
+                    title = "OLS")
 
 
+### b
+
+robust_se <- sqrt(diag(vcovHC(ols_a)))
+conf_interval <- coef(ols_a)[2] + qt(c(0.025, 0.975), df = df.residual(ols_a)) * robust_se[2] #nolint
+
+### c
+
+probit_c <- glm(survived ~ female, data = df, family = binomial(link = "probit")) #nolint
+stargazer(probit_c, type = "latex",
+                    out = "OUTPUT/probit_c.tex",
+                    label = "prbtc",
+                    title = "Probit Regression")
